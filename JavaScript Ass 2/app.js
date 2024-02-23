0a499b6d86859736e7b04b1d724bc41c84e2f2a6
AnnafYousuf <annafyousuf1@gmail.com>
2024-02-23 20:55:57 +0500

    Assignment done

---
+++ JavaScript Ass 2/app.js
@@ -0,0 +1,67 @@
+// Q1: get user to input a number using prompt () check if the answer is a multiple of 6 or not?
+
+// let num = +prompt("Enter Value");
+
+// if(num%2 === 0){
+//     console.log("this num is odd");
+// }
+// else{
+//     console.log("this num is even")
+// }
+
+// Answer 1 completed
+
+
+
+// Q2: write a code which can give grades to student according to their Marks?
+
+// let score = 94;
+// let grade;
+
+// if(score >=90 && score <=100){
+//     console.log("A+");
+// }
+// else if (score >=70 && score <= 89){
+//     console.log("A");
+// }
+// else if (score >=50 && score <= 69){
+//     console.log("B");
+// }
+// else if (score >=33 && score <= 49){
+//     console.log("C");
+// }
+// else {
+//     console.log("Fail");
+// }
+
+// Answer 2 Completed
+
+
+
+// Q3: create marksheet using if else and else if condition?
+
+// let name2 = prompt("Enter Your Name");
+// let english = +prompt("English")
+// let urdu = +prompt("Urdu")
+// let math = +prompt("Math")
+// let physics = +prompt("Physics")
+// let science = +prompt("Science")
+// let total = english + urdu + math + physics + science;
+// let percentage = (total / 500) *100;
+
+// if(percentage >=80 && percentage <=100){
+//     document.write(name2 + "your grade is A+" + percentage + "%")
+// }
+// else if(percentage >=70 && percentage <=79){
+//     document.write(name2 + "Your Grade is A" + percentage + "%")
+// }
+// else if(percentage >=50 && percentage <=69){
+//     document.write(name2 + "Your Grade is B" + percentage + "%")
+// }
+// else if(percentage >=33 && percentage <=49){
+//     document.write(name2 + "Your Grade is C" + percentage + "%")
+// }
+// else{
+//     console.log(name2+"Invalid Number"+ percentage);
+// }
+// Answer 3 Completed
